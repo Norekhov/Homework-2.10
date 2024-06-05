de0200f5e5d6cb6724dcd1e0ed8b340d8e915411
nikit <nikit.orekhoff@yandex.ru>
2024-06-05 20:54:04 +0300

    ДЗ 2.10

--- src/main/java/com/example/demo/controller/ShopController.java
+++ src/main/java/com/example/demo/controller/ShopController.java
@@ -3,13 +3,11 @@ package com.example.demo.controller;
 import com.example.demo.service.ShopService;
 import com.example.demo.shop.Shop;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RequestMapping("/order")
 @RestController
 public class ShopController {
     private final ShopService shopService;
@@ -18,12 +16,12 @@ public class ShopController {
         this.shopService = shopService;
     }
 
-    @GetMapping("/add")
+    @GetMapping("/order/add")
     public void add(@RequestParam("id") List<Integer> idProduct) {
         shopService.add(idProduct);
     }
 
-    @GetMapping("/get")
+    @GetMapping("/order/get")
     public List<Shop> get() {
         return shopService.get();
     }
